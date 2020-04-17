d0618584b2f554b850b4f6bb69a4b6a87485a5cd
rodriandrade <rodriandrade1498@gmail.com>
2020-04-17 13:00:14 -0300

    Imgur API

---
+++ rodrigoAndrade_imgur/assets/script.jsx
@@ -0,0 +1,51 @@
+const url = 'https://api.imgur.com/3/image/pVYS5Or';
+
+const getImg = async () => {
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: 'Client-ID 1bf39acc128bfab',
+        }
+    });
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+
+}
+
+const showImage = async () => {
+
+    try{
+        const showImg = await getImg();
+        const {data} = showImg;
+        const {link, title} = data;
+
+        /*
+        let img = document.createElement("img");
+        img.setAttribute("src", link);
+        img.setAttribute("width", "500px");
+        document.getElementById("imagen").append(img);
+        */
+
+        const image = <img src={link} width="900px"></img>;
+        const imageTitle = <h3>{title}</h3>
+
+        ReactDOM.render(
+            image, 
+            document.getElementById("imagen")
+        );
+
+        ReactDOM.render(
+            imageTitle, 
+            document.getElementById("titulo")
+        );
+
+    } catch(error){
+        console.log("Error al buscar la imagen", error);
+    }    
+    
+}
+
+showImage();
